fb7b8a6805b29f84d6f6d90c3df692cfc5666e58
Anton Smirnov <antosha19851985@gmail.com>
2024-06-28 12:29:22 -0600

    started with Navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
     title: "Property Pulse | Find the perfect Rental",
     description: "Find the perfect rental property in your area",
     keywords: "rental, property, real estate",
-}
+};
 
 const MainLayout = ({ children }) => {
     return (
         <html lang="en">
             <body>
-                <div>{children}</div>
+                <Navbar />
+                <main>{children}</main>
             </body>
         </html>
     );
