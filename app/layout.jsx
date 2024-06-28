f9fd8406daad33d020fc42ed1b1ea0d702f64ddd
Anton Smirnov <antosha19851985@gmail.com>
2024-06-28 11:23:05 -0600

    Revert "minor change"
    This reverts commit 41acbf47856e60bb5e5e19ad8c2f16f065064fe5.

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ const MainLayout = ({ children }) => {
     return (
         <html lang="en">
             <body>
-                <div>{children}1</div>
+                <div>{children}</div>
             </body>
         </html>
     );
